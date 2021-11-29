35e4f93fca5815cd20f1e133262d40b13f327a6c
krisjanis-a <krishjanis96@gmail.com>
2021-11-29 02:00:42 +0200

    added webpack, react

---
+++ webpack.config.cjs
@@ -0,0 +1,36 @@
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/SimpleReactUI/index.js"),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  mode: "development",
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./src/SimpleReactUI/index.html" }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, "./dist"),
+    hot: true,
+  },
+};
