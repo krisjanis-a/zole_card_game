0986030fde386f11cb7e411ef8af07305e424796
krisjanis-a <krishjanis96@gmail.com>
2021-12-01 23:54:18 +0200

    Added comments

--- gitCommit.js
+++ gitCommit.js
@@ -1,11 +1,14 @@
+// Executes arguments on commandline/powershell
 const { exec } = require("child_process");
 let args = process.argv;
+// Remove first two command line arguments ("node gitCommit.js")
 args.splice(0, 2);
-let str = args.join(" ");
+let str = args.join(" "); // Git commit message
 
 console.log(str);
 exec("git add . ", cbadd);
 
+// cbadd, cbcommit - callback functions
 function cbadd(err, stdout, stderr) {
   if (err) {
     console.log(err);
