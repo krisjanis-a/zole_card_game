7eb9810f32424878d5afe93d37ff7b55b7684cad
krisjanis-a <krishjanis96@gmail.com>
2022-01-01 19:53:53 +0200

    fix object destructuring

--- src/userInterface/store/TableCards/Table.reducer.js
+++ src/userInterface/store/TableCards/Table.reducer.js
@@ -2,9 +2,10 @@ const getInitialState = () => [];
 
 export default (state = getInitialState(), action) => {
   switch (action.type) {
-    case "SET_TABLE":
-      return action.payload;
-
+    case "SET_TABLE": {
+      const { table } = action;
+      return table;
+    }
     case "CLEAR_TABLE":
       return getInitialState();
 
