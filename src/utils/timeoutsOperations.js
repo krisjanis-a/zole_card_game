3b69546653b3506b82aa7c8efc73bb1386db0169
krisjanis-a <krishjanis96@gmail.com>
2022-01-07 23:34:04 +0200

    timeouts operations console logs removed

--- src/utils/timeoutsOperations.js
+++ src/utils/timeoutsOperations.js
@@ -1,15 +1,11 @@
 // Setter function for storage prototype
 Storage.prototype.setObject = function (key, value) {
-  // console.log(value);
-  // console.log(JSON.stringify(value));
   this.setItem(key, JSON.stringify(value));
 };
 
 // Getter function for storage prototype
 Storage.prototype.getObject = function (key) {
   var value = this.getItem(key);
-  // console.log(value);
-  // console.log(JSON.parse(value));
   return value && JSON.parse(value);
 };
 
@@ -20,24 +16,14 @@ const storage = sessionStorage;
 const addTimeoutToStorage = (timeoutID) => {
   let currentTimeouts = [];
 
-  // console.log("-|-|-|-");
-  // console.log(`TimeoutID: ${timeoutID}`);
-  // console.log(`Timeouts is null ${storage.getObject("timeouts") === null}`);
-
   if (storage.getObject("timeouts") === null) {
     currentTimeouts.push(timeoutID);
-    // console.log(`Current timeout ${currentTimeouts}`);
     storage.setObject("timeouts", currentTimeouts);
     return;
   }
 
-  // console.log("Adding to existing timeout in sessionStorage");
-
   currentTimeouts = storage.getObject("timeouts");
   currentTimeouts.push(timeoutID);
-
-  // console.log(`Current timeouts: ${currentTimeouts}`);
-  // console.log(currentTimeouts);
 
   storage.setObject("timeouts", currentTimeouts);
 };
@@ -47,7 +33,6 @@ const clearAllTimeouts = () => {
   const currentTimeouts = storage.getObject("timeouts");
   if (currentTimeouts !== null) {
     currentTimeouts.forEach((timeout) => {
-      // console.log(`Timeout for clearing ID: ${timeout}`);
       clearTimeout(timeout);
     });
   }
